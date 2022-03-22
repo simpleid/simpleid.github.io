d190ca957f5d8648f1236913cd2dcd88c8457e7b
Kelvin Mo <kmo@users.sourceforge.net>
2022-03-22 21:45:55 +1100

    Process non-HTML template files

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const escape = require('lodash.escape');
+
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(conf) {
     conf.addPlugin(pluginSyntaxHighlight);
@@ -10,6 +13,12 @@ module.exports = function(conf) {
     conf.addFilter('jsonify', function (s) {
         return JSON.stringify(s);
     });
+    conf.addFilter('xml_escape', function(s) {
+        return escape(s);
+    });
+
+    conf.addLiquidFilter('date_to_rfc822', pluginRss.dateToRfc3339);
+    conf.addLiquidFilter('getNewestCollectionItemDate', pluginRss.getNewestCollectionItemDate);
 
     return {
         dir: {
